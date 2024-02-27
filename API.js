82bb1a88a4b8fe7aeac4f9ef40f6320859df0451
OHshajim <ajshajimmax@gmail.com>
2024-02-27 20:56:32 +0600

    arrow funtion API and other comments

--- API.js
+++ API.js
@@ -1,25 +1,65 @@
-function loadData(){
+// btn API-1
+function loadData() {
     fetch('https://jsonplaceholder.typicode.com/todos/1')
-    .then(response => response.json()) // input data make this readable
-    .then(json => console.log(json))  //output as you want 
+        .then(response => response.json()) // input data make this readable
+        .then(json => console.log(json))  //output as you want 
 }
 
-
-function users (){
+// btn API-2
+function users() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(res=>res.json())
-    .then(data=>user(data)) //call a function and work as you need
+        .then(res => res.json())
+        .then(data => user(data)) //call a function and work as you need
 }
 
-function user(data){
-        data.forEach(element => {
-            console.log(element.id, element.name);
+function user(data) {
+    const ul = document.getElementById('ul');
+    data.forEach(element => {
+        console.log(element.id, element.name);
+        const li = document.createElement('li');
+        li.innerText = element.id + " " + element.name;
+        ul.appendChild(li);
     });
-    
 }
 
 
+// btn API-3 using arrow function
+const loadComment = async () => {
+    try {
+        const res = await fetch('https://jsonplaceholder.typicode.com/comments')
+        const data = await res.json();
+        console.log(data);
+    }
 
+    catch (error) {
+        console.error("data load error "); //customize error
+    }
+}
+
+
+// how we get info and display that post 
+const post = (data) => {
+    const PostBox = document.getElementById('post-box')
+    for (const post of data) {
+        console.log(post);
+        const Div = document.createElement('div');
+        Div.innerHTML = `
+        <h3>${post.id}</h3>
+        <h4>post : ${post.title}</h4>
+        <p>${post.body}</p>
+        `;
+        PostBox.appendChild(Div)
+        Div.classList.add('post')
+    }
+}
+
+function posts() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(data => post(data))
+}
+
+posts()
 
 
 
